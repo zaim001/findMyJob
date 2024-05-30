57b8c6da3e4cbd8d182a03d1bbd3bc852a29f684
zaim001 <badreddine_zaim@um5.ac.ma>
2024-05-30 17:33:54 +0100

    modified indeed.py

--- indeed.py
+++ indeed.py
@@ -1,6 +1,7 @@
 import time
 import pandas as pd
 from selenium.common import NoSuchElementException, TimeoutException
+from selenium.webdriver import Keys
 from selenium.webdriver.support.wait import WebDriverWait
 from tabulate import tabulate
 import undetected_chromedriver as uc
@@ -9,7 +10,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 
 options = Options()
-options.add_argument("--headless")
+# options.add_argument("--headless")
 options.add_argument("--start-maximized")
 options.add_argument("--lang=en-US")
 driver = uc.Chrome(options=options)
@@ -17,38 +18,40 @@ driver.get("https://www.indeed.com/")
 
 wait = WebDriverWait(driver, 5)
 
-time.sleep(5)
 job = input("Enter Job Title : ")
 location = input("Enter your Location : ")
 
 search_job = wait.until(
-    EC.presence_of_element_located((By.CSS_SELECTOR, "input#text-input-what"))
+    EC.presence_of_element_located((By.XPATH, "//input[@id='text-input-what']"))
 )
 search_job.send_keys(job)
 
 search_location = driver.find_element(By.ID, "text-input-where")
 search_location.send_keys(location)
 
-search_button = driver.find_element(By.CLASS_NAME, "yosegi-InlineWhatWhere-primaryButton")
-search_button.click()
+search_location.send_keys(Keys.ENTER)
+time.sleep(3)
 
 
 df = pd.DataFrame(columns=['Job Title', 'Company', 'Date', 'Location', 'Job Type', 'Link'])
 
 while True:
-    boxes = driver.find_elements(By.CLASS_NAME, "job_seen_beacon")
+    boxes = driver.find_elements(By.CSS_SELECTOR, "div.job_seen_beacon")
     for b in boxes:
-        link = b.find_element(By.CSS_SELECTOR, "table tbody tr td div h2 a").get_attribute('href')
-        job_title = b.find_element(By.CSS_SELECTOR, "table tbody tr td div h2 a span[title]").text
-        company = b.find_element(By.CSS_SELECTOR, "table tbody tr td div div span[data-testid='company-name']").text
-        date_posted = b.find_element(By.CSS_SELECTOR, "div.underShelfFooter div span").text
-        date_posted = date_posted.replace("Posted", "")
-        date_posted = date_posted.replace("Employer", "")
-        job_location = b.find_element(By.CSS_SELECTOR, "table tbody tr td div div div").text
+        link = b.find_element(By.CSS_SELECTOR, "a.jcs-JobTitle").get_attribute('href')
+        job_title = b.find_element(By.CSS_SELECTOR, "span[title]").text
         try:
-            job_type = b.find_element(By.CSS_SELECTOR, "table.big6_visualChanges tbody tr td div.jobMetaDataGroup div div div").text
+            company = b.find_element(By.CSS_SELECTOR, "span[data-testid='company-name']").text
         except NoSuchElementException:
-            job_type = "None"
+            company = "_"
+        date_posted = b.find_element(By.CSS_SELECTOR, "span[data-testid='myJobsStateDate']").text
+        date_posted = date_posted.replace("Posted", "").strip()
+        date_posted = date_posted.replace("Employer", "").strip()
+        job_location = b.find_element(By.CSS_SELECTOR, "div[data-testid='text-location']").text
+        try:
+            job_type = b.find_element(By.CSS_SELECTOR, "div[data-testid='attribute_snippet_testid']").text
+        except NoSuchElementException:
+            job_type = "_"
 
         data = pd.DataFrame({'Job Title': [job_title], 'Company': [company],'Date': [date_posted], 'Location':[job_location], 'Job Type': [job_type], 'Link': [link]})
         df = pd.concat([df, data], ignore_index=True)
@@ -57,17 +60,14 @@ while True:
         driver.get(next_page)
         try:
             dialog = WebDriverWait(driver, 5).until(
-                EC.presence_of_element_located((By.CSS_SELECTOR, "div#mosaic-desktopserpjapopup div button"))
-            )
+                    EC.presence_of_element_located((By.CSS_SELECTOR, "div#mosaic-desktopserpjapopup div button"))
+                )
             dialog.click()
         except TimeoutException:
             pass
-
-        time.sleep(1)
+        time.sleep(2)
     except NoSuchElementException:
-        print("No more pages")
         break
-    break
 
 if df.empty:
     print(f"No Data Found for {job} in {location}")
@@ -75,5 +75,6 @@ else:
     print(tabulate(df, headers='keys', tablefmt='pretty'))
     df.to_csv('indeed.csv', index=False)
     df.to_excel('indeed.xlsx', index=False)
+
 
 driver.quit()
